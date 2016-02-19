b1f6161eba9b91f21545b9125f332cb6097d1c68
James Hayes <james.s.hayes@gmail.com>
2016-02-19 15:53:22 -0700

    Added a logger for logging User and browser

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,12 +8,60 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+	private function browser_name()
+	{
+		$ua = $_SERVER['HTTP_USER_AGENT'];
+
+		if (
+			strpos(strtolower($ua), 'safari/') &&
+			strpos(strtolower($ua), 'opr/')
+		) {
+			// Opera
+			$res = 'Opera';
+		} elseif (
+			strpos(strtolower($ua), 'safari/') &&
+			strpos(strtolower($ua), 'chrome/')
+		) {
+			// Chrome
+			$res = 'Chrome';
+		} elseif (
+			strpos(strtolower($ua), 'msie') ||
+			strpos(strtolower($ua), 'trident/')
+		) {
+			// Internet Explorer
+			$res = 'Internet Explorer';
+		} elseif (strpos(strtolower($ua), 'firefox/')) {
+			// Firefox
+			$res = 'Firefox';
+		} elseif (
+			strpos(strtolower($ua), 'safari/') &&
+			(strpos(strtolower($ua), 'opr/') === false) &&
+			(strpos(strtolower($ua), 'chrome/') === false)
+		) {
+			// Safari
+			$res = 'Safari';
+		} else {
+			// Out of data
+			$res = false;
+		}
+
+		return $res;
+	}
+	
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
 		if ($this->get('security.context')->isGranted('ROLE_STAFF')) {
+			
+			$user_name = $this->get('security.context')->getToken()->getUser()->getUsername();
+			$browser_name = $this->browser_name();
+			
+			$logger = $this->get('app.logger');
+			
+			$logger->info("User: ".$user_name." - ".$browser_name." - ".date("Y-m-d G:i:s T"));
+			
 			return $this->render(
 				'default/index.html.twig',
 				array()
