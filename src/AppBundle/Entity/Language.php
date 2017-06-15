da35b6d03ddedec747502a02935f0baacb353e31
James Schofield Hayes <james.s.hayes@gmail.com>
2017-06-15 16:59:04 -0600

    Added pointer headers to the LICENSE file the LICENSE file contains the EPLv1.0 License

--- src/AppBundle/Entity/Language.php
+++ src/AppBundle/Entity/Language.php
@@ -1,9 +1,14 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  This file is part of BaseTerm.
+ *  
+ *  BaseTerm is an open-source and free to use terminology management system built with the primary goal of natively supporting the most popular TBX dialect for exchange, TBX-Basic
+ *  
+ *  Copyright © (2016-2017) BYU TRG & LTAC Global & CRITI
+ *  
+ *  See the file LICENSE file that accompanied this source code for the full license
+ *  information
  */
 
 namespace AppBundle\Entity;
