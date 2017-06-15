472a106a4320995e2da7b8262ec58c4fc1a82819
James Schofield Hayes <james.s.hayes@gmail.com>
2017-06-15 16:59:04 -0600

    Added pointer headers to the LICENSE file the LICENSE file contains the EPLv1.0 License

--- src/AppBundle/Entity/Region.php
+++ src/AppBundle/Entity/Region.php
@@ -1,5 +1,14 @@
 <?php
-
+/*
+ *  This file is part of BaseTerm.
+ *  
+ *  BaseTerm is an open-source and free to use terminology management system built with the primary goal of natively supporting the most popular TBX dialect for exchange, TBX-Basic
+ *  
+ *  Copyright © (2016-2017) BYU TRG & LTAC Global & CRITI
+ *  
+ *  See the file LICENSE file that accompanied this source code for the full license
+ *  information
+ */
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
--- src/UserBundle/Form/Type/ProfileFormType.php
+++ src/UserBundle/Form/Type/ProfileFormType.php
@@ -8,6 +8,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+/*
+ *  This file is part of BaseTerm.
+ *  
+ *  BaseTerm is an open-source and free to use terminology management system built with the primary goal of natively supporting the most popular TBX dialect for exchange, TBX-Basic
+ *  
+ *  Copyright © (2016-2017) BYU TRG & LTAC Global & CRITI
+ *  
+ *  See the file LICENSE file that accompanied this source code for the full license
+ *  information
+ */
 
 namespace UserBundle\Form\Type;
 
